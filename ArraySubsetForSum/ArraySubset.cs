976b2f218ea1af81a7bcee432b7e2ee61cf8f232
Neeraj Yadav <er.neerajyadav@gmail.com>
2018-07-26 10:07:22 +0530

    init comit

---
+++ ArraySubsetForSum/ArraySubset.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraySubsetForSum
+{
+    public static class ArraySubset
+    {
+        public static void FindRequiredSumSubArray(int[] arr, int sum)
+        {
+            //create an array for the subset with max length of input array  
+            int[] sub = new int[arr.Length];
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i, col = 0; j < arr.Length; j++, col++)
+                {
+                    //add the value of input array one by one  
+                    temp += arr[j];
+                    sub[col] = arr[j];
+                    //if addition is equal to sum then print it  
+                    if (temp == sum)
+                    {
+                        int total = 0;
+                        for (int k = 0; k < sub.Length; k++)
+                        {
+                            total += sub[k];
+                            Console.Write(sub[k].ToString() + " ");
+
+                            //if total and sum are equal then leave the print  
+                            if (total == sum)
+                            {
+                                Console.Write("\n");
+                                break;
+                            }
+                        }
+                    }
+                    //if temp is greater than sum are equal then clear the sub array, set temp value and leave the loop for next  
+                    if (temp > sum)
+                    {
+                        Array.Clear(sub, 0, sub.Length);
+                        temp = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- ArraySubsetForSum/Program.cs
+++ ArraySubsetForSum/Program.cs
@@ -6,7 +6,13 @@ namespace ArraySubsetForSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), bTemp => Convert.ToInt32(bTemp));
+
+            int sum = Convert.ToInt32(Console.ReadLine());
+
+            ArraySubset.FindRequiredSumSubArray(array, sum);
+
+            Console.ReadKey();
         }
     }
 }
